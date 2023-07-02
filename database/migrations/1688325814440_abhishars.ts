b5075351a841a545a1900c10271a737f46aed3d1
sohan2410 <bandarysohan24@gmail.com>
2023-07-03 01:46:30 +0530

    added abhishar API

--- database/migrations/1688325814440_abhishars.ts
+++ database/migrations/1688325814440_abhishars.ts
@@ -1,11 +1,17 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'blogs'
+  protected tableName = 'abhishars'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.string('heading')
+      table.string('version')
+      table.string('text')
+      table.string('image')
+      table.string('pdf_link')
+      table.date('date_of_launch')
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
@@ -15,7 +21,7 @@ export default class extends BaseSchema {
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -2,6 +2,7 @@ import Route from '@ioc:Adonis/Core/Route'
 import '../Routes/Auth'
 import '../Routes/Event'
 import '../Routes/Blog'
-Route.get('/', async ({ view }) => {
-  return view.render('index')
+import '../Routes/Abhishar'
+Route.get('/', async ({}) => {
+  return { message: 'Server is up and running' }
 })
