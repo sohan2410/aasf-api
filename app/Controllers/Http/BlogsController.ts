63fd3fdc67587909aff106b8412bb3400e6eabaa
navvvgupta <abhi2017gupta@gmail.com>
2023-07-16 16:31:29 +0530

    redis added using ioredis

--- app/Controllers/Http/BlogsController.ts
+++ app/Controllers/Http/BlogsController.ts
@@ -2,22 +2,18 @@
 import User from 'App/Models/User'
 import { parse } from 'rss-to-json'
 import Env from '@ioc:Adonis/Core/Env'
-import { createClient } from 'redis'
-const redisClient=createClient();
-(async ()=>{
-  redisClient.on('error', err => console.log('Redis Client Error', err));
-  await redisClient.connect();
-})();
-const DEFAULT_EXPIRATION=3600;
+import Redis from '@ioc:Adonis/Addons/Redis'
+
+const DEFAULT_EXPIRATION=3600*12;
 
 export default class BlogsController {
   public async index() {
-    const cachedData=await redisClient.get('blogs')
+    const cachedData=await Redis.get('blogs')
     if(cachedData){
       return User.getResponse(1, 'blog.fetched',JSON.parse(cachedData));
     }
     const blogs = await parse('https://medium.com/feed/tech-iiitg', { headers: { Authorization: `Bearer ${Env.get('MEDIUM_TOKEN')}` } })
-    await redisClient.setEx('blogs',DEFAULT_EXPIRATION,JSON.stringify(blogs));
+    await Redis.set('blogs',JSON.stringify(blogs),"EX",DEFAULT_EXPIRATION);
     return User.getResponse(1, 'blog.fetched', blogs)
   }
 }
