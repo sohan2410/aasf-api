dc8905dfee6c74cc763b8d9502c86ad942636619
navvvgupta <abhi2017gupta@gmail.com>
2023-07-21 00:08:26 +0530

    variable 'cachedData' name changed

--- app/Controllers/Http/BlogsController.ts
+++ app/Controllers/Http/BlogsController.ts
@@ -8,9 +8,9 @@ const DEFAULT_EXPIRATION=3600*12;
 
 export default class BlogsController {
   public async index() {
-    const cachedData=await Redis.get('blogs')
-    if(cachedData){
-      return User.getResponse(1, 'blog.fetched',JSON.parse(cachedData));
+    const blogsCache=await Redis.get('blogs')
+    if(blogsCache){
+      return User.getResponse(1, 'blog.fetched',JSON.parse(blogsCache));
     }
     const blogs = await parse('https://medium.com/feed/tech-iiitg', { headers: { Authorization: `Bearer ${Env.get('MEDIUM_TOKEN')}` } })
     await Redis.set('blogs',JSON.stringify(blogs),"EX",DEFAULT_EXPIRATION);
