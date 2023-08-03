3fef38240c7a1b7351f9b5bd28796b41eccb2aeb
navvvgupta <abhi2017gupta@gmail.com>
2023-08-04 02:19:39 +0530

    added middleware

--- Routes/Attendance.ts
+++ Routes/Attendance.ts
@@ -2,7 +2,6 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   Route.post('/attendance', 'AttendanceController.store').middleware('auth')
-  Route.get('/qr/:id/:day', 'AttendanceController.index').middleware(['auth', 'admin'])
+  Route.get('/qr/:id/:day', 'AttendanceController.index').middleware('auth')
   Route.post('/attendance/upload', 'AttendanceController.upload').middleware(['auth', 'admin'])
-  // .middleware(['auth', 'admin'])
 })
