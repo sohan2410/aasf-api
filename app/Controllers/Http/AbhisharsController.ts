366feaa80610197c9c75f3439441ebfb8ec7bc6b
pranavz <pranavzinzurde@gmail.com>
2023-08-04 02:34:00 +0530

    add abhishar static serving

--- app/Controllers/Http/AbhisharsController.ts
+++ app/Controllers/Http/AbhisharsController.ts
@@ -1,13 +1,12 @@
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Drive from '@ioc:Adonis/Core/Drive'
+// import Drive from '@ioc:Adonis/Core/Drive'
 
-import Application from '@ioc:Adonis/Core/Application'
-import Abhishar from 'App/Models/Abhishar'
+// import Application from '@ioc:Adonis/Core/Application'
+import Abhishar from '../../../abhishar'
 import User from 'App/Models/User'
 
 export default class AbhisharsController {
   public async index() {
-    const abhishars = await Abhishar.all()
-    return User.getResponse(1, 'abhishar.fetched', abhishars)
+    return User.getResponse(1, 'abhishar.fetched', Abhishar)
   }
 }
