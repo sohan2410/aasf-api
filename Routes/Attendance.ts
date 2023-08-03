e8122009292f275bee2bc6c1ae06e82d62c4796f
navvvgupta <abhi2017gupta@gmail.com>
2023-08-04 03:51:25 +0530

    minor changes

--- Routes/Attendance.ts
+++ Routes/Attendance.ts
@@ -2,6 +2,6 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   Route.post('/attendance', 'AttendanceController.store').middleware('auth')
-  Route.get('/qr/:id/:day', 'AttendanceController.index').middleware('auth')
+  Route.get('/qr/:id/:day', 'AttendanceController.index').middleware(['auth', 'admin'])
   Route.post('/attendance/upload', 'AttendanceController.upload').middleware(['auth', 'admin'])
 })
--- Routes/Blog.ts
+++ Routes/Blog.ts
@@ -1,5 +1,5 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-  Route.resource('blogs', 'BlogsController').middleware('auth')
+  Route.resource('blogs', 'BlogsController')
 })
