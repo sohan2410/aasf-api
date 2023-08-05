73f43c8fc4339b399ae9854ee1e252ecb05afb97
navvvgupta <abhi2017gupta@gmail.com>
2023-08-05 16:47:15 +0530

    Google Sheets pushed

--- app/Controllers/Http/ReportBugsController.ts
+++ app/Controllers/Http/ReportBugsController.ts
@@ -2,31 +2,42 @@
 import axios from 'axios'
 import { google } from 'googleapis'
 import User from 'App/Models/User'
-
-const GOOGLE_FORM_URL = 'https://forms.gle/k6ZpicUF7TLiZoG9A'
+import Env from '@ioc:Adonis/Core/Env'
 
 export default class ReportBugsController {
     public async store({ request, auth }) {
         try {
-            const { formResponse } = request.all()
-          console.log(formResponse);
+          const {Name}=request.all()
           
-            const client_id ='795549443504-05mg37epsqfo48n9aspmt134khnupb1h.apps.googleusercontent.com'
-            const client_secret = 'V4shc2BBXbnPQrT-1_3hI5JC'
-            const redirect_uris = 'https://developers.google.com/oauthplayground'
-            const REFRESH_TOKEN ='1//04NO-41FG_e2xCgYIARAAGAQSNwF-L9Ir91qLzkQVNety2Jom02CUllNzoJP7ORXRPT0MT1lk42mVSpsAivjRDA3qJ3V-wC7Ta8Q'
+          const formResponse = {
+            Name: Name,
+          };
+            //declare OAuth Credentials
+            const client_id =Env.get('OAUTH_CLIENT_ID')
+            // console.log(client_id);
+            
+            const client_secret = Env.get('OAUTH_CLIENT_SECRET')
+            const redirect_uris = Env.get('OAUTH_REDIRECT_URI')
+            const REFRESH_TOKEN =Env.get('OAUTH_REFRESH_TOKEN')
       
             // Set up OAuth2 client
             const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris)
-            oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN }) // Replace with your refresh token
+            oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN }) 
       
+            const SCOPES = ['https://www.googleapis.com/auth/forms'];
+            const authUrl = oAuth2Client.generateAuthUrl({
+            access_type: 'offline',
+            scope: 'https://www.googleapis.com/auth/spreadsheets',
+            });
+          const values = Object.values(formResponse);
+
             // Submit the form response
-            const sheets = google.sheets({ version: 'v4', auth: oAuth2Client })
-            await sheets.spreadsheets.values.append({
-                spreadsheetId: 'https://docs.google.com/spreadsheets/d/1G86LjeIK_GuucSY5lcbVfNUiTn9Ei4TvmuPj5mfdMTA/edit?usp=sharing', // Replace with your spreadsheet ID
-                range: 'Form Responses 1', // This might vary depending on your form configuration
-                valueInputOption: 'RAW',
-                requestBody: { values: [formResponse] },
+          const sheets = google.sheets({ version: 'v4', auth: oAuth2Client })
+          await sheets.spreadsheets.values.append({
+              spreadsheetId: '1G86LjeIK_GuucSY5lcbVfNUiTn9Ei4TvmuPj5mfdMTA',//spreadsheetId
+              range: 'Form Responses 1!A2', // This might vary depending on form configuration
+              valueInputOption: 'RAW',
+              requestBody: { values: [values] },
               })
       
             return User.getResponse(1, 'Bug Reported')
