232584cfae5de1fea8036586d9a1169d808c67cc
Abhinav Gupta <115642649+navvvgupta@users.noreply.github.com>
2023-08-30 18:34:30 +0530

    add middleware (#15)
    * added middleware
    
    * middleware added
    
    * minor changes

--- Routes/Attendance.ts
+++ Routes/Attendance.ts
@@ -4,5 +4,4 @@ Route.group(() => {
   Route.post('/attendance', 'AttendanceController.store').middleware('auth')
   Route.get('/qr/:id/:day', 'AttendanceController.index').middleware(['auth', 'admin'])
   Route.post('/attendance/upload', 'AttendanceController.upload').middleware(['auth', 'admin'])
-  // .middleware(['auth', 'admin'])
 })
